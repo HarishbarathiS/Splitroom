d1517a3579b13dffa904fbc55d2070fdaa6a87c0
unknown <you@example.com>
2024-06-03 17:11:16 -0400

    Backend code for Splitroom

---
+++ app.py
@@ -0,0 +1,94 @@
+# backend logic : 
+
+
+def getNames(n):
+    names = []
+    for i in range(n):
+        name = input(f"Enter name {i+1} : ")
+        names.append(name)
+    return names
+
+def getDeets():
+    deets = []
+    Date = input("Date of expense : ")
+    type = input("Type of expense : ")
+    total = int(input("Total amount spent : "))
+    deets.extend([Date,type,total])
+    return deets
+
+def PaidBy(names):
+    paid = {}
+    print("Contribution : (name - amount)")
+    while True:
+        Name_amt = input()
+        if Name_amt:
+            Name_amt = Name_amt.split(" ")
+            paid[Name_amt[0]] = int(Name_amt[1])
+        else:
+            break
+    return paid
+    
+def SplitEqually(total,n,paid,names):
+    split = total/n;
+    for name in names:
+        if name not in paid:
+            paid[name] = 0.0
+
+        paid[name] -= split
+    sorted_paid = {k : v for k,v in sorted(paid.items(), key=lambda item : item[1],reverse=True)}
+    # converting dictionary into list
+    dict_items = list(sorted_paid.items())
+    i = 0
+    j = len(dict_items) - 1
+    #print(dict_items)
+    
+    while i < j:
+        i_item = dict_items[i]
+        i_key,i_value = i_item
+
+        j_item = dict_items[j]
+        j_key,j_value = j_item 
+        flag = 1
+        if i_value + j_value > 0:
+            i_value = round(i_value + j_value,2)
+            print(f"{j_key} owes {i_key} ${round(-j_value,2)}")
+            j_value = 0
+        else:
+            j_value = round(j_value + i_value,2)
+            print(f"{j_key} owes {i_key} ${round(i_value,2)}")
+            i_value = 0;
+            flag = 0
+        
+        m_i_item = (i_key,i_value)
+        m_j_item = (j_key,j_value)
+        dict_items[i] = m_i_item
+        dict_items[j] = m_j_item
+
+        if flag:
+            j -= 1
+        else:
+            i += 1
+
+    #print(dict_items)
+
+
+
+print("Welcome to Splitroom")
+n = int(input("Enter number of people : "))
+names = getNames(n)
+deets = getDeets()
+paid = PaidBy(names)
+SplitEqually(deets[2],n,paid,names)
+
+
+
+
+
+
+
+
+
+
+
+
+#n = int(input("Enter the total number of people : "))
